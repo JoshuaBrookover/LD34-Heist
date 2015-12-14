b6008d9e12c0575e81bbc88e22a4ca59ddd9f4d2
Tim Ullrich <tullrich@nerdkingdom.com>
2015-12-13 18:51:12 -0600

    Dialog skip.

--- Assets/Scripts/AutoType.cs
+++ Assets/Scripts/AutoType.cs
@@ -17,13 +17,32 @@ public class AutoType : MonoBehaviour {
 	public UnityEvent myEvent;
  
 	string message;
+
+	private IEnumerator typeCoroutine;
+
+	enum DialogStage 
+	{ 
+		NotStarted, 
+		Typing, 
+		Pausing, 
+		Done 
+	};
+	private DialogStage stage = DialogStage.NotStarted;
  
 	// Use this for initialization
 	void Start () {
 
 		message = GetComponent<Text>().text;
 		GetComponent<Text>().text = "";
-		StartCoroutine(TypeText ());
+
+		typeCoroutine = DoDialogStage(true);
+		StartCoroutine( typeCoroutine );
+	}
+
+	void Update() {
+		if ((stage == DialogStage.Typing || stage == DialogStage.Pausing) && Input.GetButtonDown("SkipDialog")) {
+			Skip();
+		}
 	}
 
 	void HideAllSpecials()	{
@@ -31,42 +50,80 @@ public class AutoType : MonoBehaviour {
 			special.SetActive(false);
 		}
 	}
- 
-	IEnumerator TypeText () {
 
-		yield return new WaitForSeconds(startPause);
+	void ShowAllSpecials()	{
+		foreach (GameObject special in specials) {
+			special.SetActive(true);
+		}
+	}
 
-		int specialCount = 0;
+	IEnumerator DoDialogStage(bool typewriter) {
+		if ( typewriter ) {
+			stage = DialogStage.Typing;
+			yield return new WaitForSeconds(startPause);
+
+			int specialCount = 0;
+			foreach (char letter in message.ToCharArray()) {
+				if (letter == '@')
+				{
+					yield return new WaitForSeconds (symbolPause);
+					specials[specialCount++].SetActive(true);
+				}
+				else
+				{
+					GetComponent<Text>().text += letter;
+				}
+
+				if (letter != ' ')
+				{
+					if (sound)
+						GetComponent<AudioSource>().PlayOneShot (sound);
+						yield return 0;
+
+					if ( letter == '\n' ) {
+						yield return new WaitForSeconds (linePause);
+
+					}
+					else {
+						yield return new WaitForSeconds (letterPause);
+					}
+				}
+			}      
+		} else {
+			ShortCutText();
+		}
+
+		stage = DialogStage.Pausing;
+		yield return new WaitForSeconds(nextPause);
+		Complete();
+	}
+
+	void Complete() {
+		stage = DialogStage.Done;
+		gameObject.SetActive(false);
+		myEvent.Invoke();
+	}
+
+	void ShortCutText() {
+		ShowAllSpecials();
+		GetComponent<Text>().text = "";
 		foreach (char letter in message.ToCharArray()) {
-			if (letter == '@')
-			{
-				yield return new WaitForSeconds (symbolPause);
-				specials[specialCount++].SetActive(true);
-			}
-			else
+			if (letter != '@')
 			{
 				GetComponent<Text>().text += letter;
 			}
-
-			if (letter != ' ')
-			{
-				if (sound)
-					GetComponent<AudioSource>().PlayOneShot (sound);
-					yield return 0;
-
-				if ( letter == '\n' ) {
-					yield return new WaitForSeconds (linePause);
-
-				}
-				else {
-					yield return new WaitForSeconds (letterPause);
-				}
-			}
 		}      
+	}
 
-		yield return new WaitForSeconds(nextPause);
+	void Skip() {
+		if (stage == DialogStage.Typing) {
+			StopCoroutine(typeCoroutine);
+			typeCoroutine = DoDialogStage(false);
+			StartCoroutine(typeCoroutine);
+		} else if (stage == DialogStage.Pausing) {
+			StopCoroutine(typeCoroutine);
 
-		gameObject.SetActive(false);
-		myEvent.Invoke();
+			Complete();
+		}
 	}
 }
