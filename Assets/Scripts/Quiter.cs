fbcafb1a3ca339880c7bc7d7e44dfe706d67acf1
Tim Ullrich <tullrich@nerdkingdom.com>
2015-12-13 18:54:59 -0600

    escape key kills app anywhere

---
+++ Assets/Scripts/Quiter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quiter : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Quit")) {
+			Application.Quit();
+		}
+	}
+}
